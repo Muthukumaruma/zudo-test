f73e5de5104561a25305acef877181df8807fd6a
muthu <muthukumar@theviralfever.com>
2020-06-30 22:27:47 +0530

    pm2 echo file added

---
+++ movie/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: "MovieApp PM2",
+        script: "server.js",
+        error_file: "/dev/stdout",
+        out_file: "/dev/stdout",
+        merge_logs: true,
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        instances: "max",
+        exec_mode: "cluster",
+        autorestart: true,
+        restart_delay: 5000,
+        watch: false,
+        max_memory_restart: "1G",
+        env: {
+          NODE_ENV: "production"
+        }
+      }
+    ]
+  };
+  
